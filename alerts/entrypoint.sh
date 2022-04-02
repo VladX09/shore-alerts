2a1f8fe5b25d30be37463c3fbc9a9b3b632013b4
laoladis <vladx09rus@gmail.com>
2022-04-02 18:56:28 +0000

    Add admin superuser generation

--- alerts/entrypoint.sh
+++ alerts/entrypoint.sh
@@ -5,6 +5,7 @@ case "$1" in
     start)
         python manage.py makemigrations
         python manage.py migrate
+        python manage.py createsuperuser --noinput
         python manage.py runserver 0.0.0.0:8080
         ;;
     *)
