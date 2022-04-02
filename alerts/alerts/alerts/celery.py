6806d8aca63513db37daaaf343a3134b74d9606f
laoladis <vladx09rus@gmail.com>
2022-04-02 17:57:19 +0000

    Add common_lib installation

--- alerts/alerts/alerts/celery.py
+++ alerts/alerts/alerts/celery.py
@@ -6,6 +6,6 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alerts.settings")
 
 
-app = Celery("alerts")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+celery_app = Celery("alerts")
+celery_app.config_from_object("django.conf:settings", namespace="CELERY")
+celery_app.autodiscover_tasks()
