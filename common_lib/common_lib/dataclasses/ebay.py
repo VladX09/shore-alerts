ab1868b7b15b67d706a68fe40ca5380233761f8d
laoladis <vladx09rus@gmail.com>
2022-04-02 21:36:12 +0000

    Add ebay item model

---
+++ common_lib/common_lib/dataclasses/ebay.py
@@ -0,0 +1,44 @@
+import typing as t
+from dataclasses import dataclass
+
+from rest_framework import serializers
+
+
+class EbayImageSummarySchema(serializers.Serializer):
+    imageUrl = serializers.CharField()
+
+
+class EbayPriceSchema(serializers.Serializer):
+    value = serializers.FloatField(required=False)
+    currency = serializers.CharField(required=False)
+
+
+class EbayItemSummarySchema(serializers.Serializer):
+    title = serializers.CharField()
+    image = EbayImageSummarySchema()
+    itemHref = serializers.URLField()
+    price = EbayPriceSchema()
+
+
+@dataclass
+class EbayItem:
+    title: str
+    href: str
+    image_url: str
+    price: t.Optional[str]
+    currency: t.Optional[str]
+
+    @classmethod
+    def parse(cls, data: t.Dict[str, t.Any]):
+        schema = EbayItemSummarySchema(data=data)
+
+        schema.is_valid(raise_exception=True)
+        validated_data = schema.validated_data
+
+        return cls(
+            title=validated_data["title"],
+            href=validated_data["itemHref"],
+            image_url=validated_data["image"]["imageUrl"],
+            price=validated_data["price"].get("value"),
+            currency=validated_data["price"].get("currency"),
+        )
