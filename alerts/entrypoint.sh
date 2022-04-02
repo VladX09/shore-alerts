cd775cb507effb4aca3cdfbb310737a6771bd3c3
laoladis <vladx09rus@gmail.com>
2022-04-02 18:44:31 +0000

    Add entrypoint

---
+++ alerts/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+case "$1" in
+    start)
+        python manage.py makemigrations
+        python manage.py migrate
+        python manage.py runserver 0.0.0.0:8080
+        ;;
+    *)
+        exec "$@"
+        ;;
+esac
