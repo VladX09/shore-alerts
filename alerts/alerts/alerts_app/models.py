2b03de8ac3b70912c713f7059a4ed2248b33864d
Ladislao <vladx09rus@gmail.com>
2022-04-03 16:14:03 +0000

    Move model signals logic to views

--- alerts/alerts/alerts_app/models.py
+++ alerts/alerts/alerts_app/models.py
@@ -1,11 +1,7 @@
-import json
 import typing as t
-import uuid
 
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.db.models.signals import post_delete, pre_save
-from django.dispatch import receiver
 from django_celery_beat.models import IntervalSchedule, PeriodicTask
 
 
@@ -48,33 +44,3 @@ class Alert(BaseModel):
         return (
             f"Alert({self.id}, {self.email}, {self.query}, {self.every}, {self.period})"
         )
-
-
-@receiver(pre_save, sender=Alert)
-def alert_pre_save(sender, instance, *args, **kwargs):
-    interval, _ = IntervalSchedule.objects.get_or_create(
-        every=instance.every,
-        period=instance.period,
-    )
-
-    task_id = str(uuid.uuid4())
-
-    task = PeriodicTask.objects.create(
-        interval=interval,
-        name=task_id,
-        task="alerts_app.tasks.compose_and_send_alert",
-        kwargs=json.dumps(
-            {
-                "task_id": task_id,
-            }
-        ),
-    )
-
-    instance.task = task
-
-
-@receiver(post_delete, sender=Alert)
-def alert_post_delete(sender, instance, *args, **kwargs):
-    """Delete task on alert delete."""
-    if instance.task:
-        instance.task.delete()
--- alerts/alerts/alerts_app/views.py
+++ alerts/alerts/alerts_app/views.py
@@ -1,8 +1,64 @@
+import json
+import uuid
+from logging import getLogger
+
+from django.db import transaction
+from django_celery_beat.models import IntervalSchedule, PeriodicTask
 from rest_framework import viewsets
 
 from . import models, serializers
 
 
+logger = getLogger(__name__)
+
+
 class AlertViewSet(viewsets.ModelViewSet):
     queryset = models.Alert.objects.all()
     serializer_class = serializers.Alert
+
+    def perform_create(self, serializer):
+        with transaction.atomic():
+            data = serializer.validated_data
+
+            interval, _ = IntervalSchedule.objects.get_or_create(
+                every=data["every"],
+                period=data["period"],
+            )
+
+            task_id = str(uuid.uuid4())
+
+            task = PeriodicTask.objects.create(
+                interval=interval,
+                name=task_id,
+                task="alerts_app.tasks.compose_and_send_alert",
+                kwargs=json.dumps(
+                    {
+                        "task_id": task_id,
+                    }
+                ),
+            )
+
+            logger.debug("Adding task %s", task)
+            serializer.save(task=task)
+
+    def perform_update(self, serializer):
+        with transaction.atomic():
+            instance = serializer.save()
+
+            interval, _ = IntervalSchedule.objects.get_or_create(
+                every=instance.every,
+                period=instance.period,
+            )
+
+            logger.debug("Updating task %s", instance.task)
+
+            instance.task.interval = interval
+            instance.task.save()
+
+    def perform_destroy(self, instance):
+        task = instance.task
+        instance.delete()
+
+        logger.debug("Removing task %s", instance.task)
+        if task:
+            task.delete()
