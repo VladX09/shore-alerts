1dfd4b69208a821ca679b772a6401b3207b29b88
Ladislao <vladx09rus@gmail.com>
2022-04-03 02:31:44 +0000

    Connect alert with task model

--- alerts/alerts/alerts_app/models.py
+++ alerts/alerts/alerts_app/models.py
@@ -1,16 +1,19 @@
 from django.db import models
-from django_celery_beat.models import IntervalSchedule
+from django_celery_beat.models import PeriodicTask
 
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
+
 
 class Alert(BaseModel):
     email = models.EmailField()
     query = models.TextField()
-    interval = models.ForeignKey(IntervalSchedule, on_delete=models.CASCADE)
+    task = models.OneToOneField(PeriodicTask, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f"Alert({self.id}, {self.email}, {self.query}, {self.interval})"
+        return f"Alert({self.id}, {self.email}, {self.query}, {self.task.interval})"
--- alerts/alerts/alerts_app/serializers.py
+++ alerts/alerts/alerts_app/serializers.py
@@ -1,15 +1,53 @@
+import json
+
+from django.db import transaction
+from django_celery_beat import models as beat_models
 from rest_framework import serializers
 
 from . import models
 
 
-class Alert(serializers.ModelSerializer):
-    class Meta:
-        model = models.Alert
-        fields = ["id", "email", "query", "interval"]
+class Alert(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    email = serializers.EmailField()
+    query = serializers.CharField()
+    every = serializers.IntegerField(min_value=1)
+    period = serializers.ChoiceField(
+        choices=beat_models.IntervalSchedule.PERIOD_CHOICES
+    )
 
+    def create(self, validated_data):
+        with transaction.atomic():
+            interval, _ = beat_models.IntervalSchedule.objects.get_or_create(
+                every=validated_data["every"],
+                period=validated_data["period"],
+            )
+            task = beat_models.PeriodicTask.objects.create(
+                interval=interval,
+                name="Send alert",
+                task="alerts_app.tasks.compose_and_send_alert",
+            )
+            alert = models.Alert.objects.create(
+                email=validated_data["email"],
+                query=validated_data["query"],
+                task=task,
+            )
+            alert.task.kwargs = json.dumps(
+                {
+                    "alert_id": alert.id,
+                }
+            )
+            alert.task.save()
+            return alert
 
-class IntervalSchedule(serializers.ModelSerializer):
-    class Meta:
-        model = models.IntervalSchedule
-        fields = ["id", "every", "period"]
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
+
+    def to_representation(self, instance):
+        ret = {}
+        ret["id"] = instance.id
+        ret["email"] = instance.email
+        ret["query"] = instance.query
+        ret["every"] = instance.task.interval.every
+        ret["period"] = instance.task.interval.period
+        return ret
