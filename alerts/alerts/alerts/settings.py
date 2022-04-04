a169a1b98e0ffd4a305cdb476a36215fb74d2d9b
Ladislao <vladx09rus@gmail.com>
2022-04-04 23:08:39 +0000

    Export email port

--- alerts/alerts/alerts/settings.py
+++ alerts/alerts/alerts/settings.py
@@ -155,7 +155,7 @@ CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
 # Email options
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
