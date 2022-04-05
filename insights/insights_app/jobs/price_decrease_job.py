e04f67c3a66a783bdeeca7b7642fb242de25a4e4
Ladislao <vladx09rus@gmail.com>
2022-04-05 00:24:33 +0000

    Add data preparation operations

--- insights/insights_app/jobs/price_decrease_job.py
+++ insights/insights_app/jobs/price_decrease_job.py
@@ -1,18 +1,78 @@
+import collections
+import itertools
 import logging
 import smtplib
 import time
+import typing as t
 
+import arrow
 import click
 from crontab import CronTab
 from insights_app.alerts_client import AlertsClient
 from insights_app.smtp_client import smtp_client
 
 
+INSIGHTS_DELTA = {"days": -2}
 logger = logging.getLogger(__name__)
 
+DJANGO_DT_FORMAT = ""  # TODO:
 
-def price_decrease_job(alert_client: AlertsClient, smtp: smtplib.SMTP):
+
+def select_price_minmax(from_dt: arrow.Arrow, alerts_client: AlertsClient):
+    # Get lists of records for each combination of email, item_id and currency
+    # Sorted by updated_date (ascended)
+
+    results = AlertsClient.get_alert_items(
+        updated_at__gte=from_dt.format(DJANGO_DT_FORMAT),
+        ordering="alert__email,item_id,currency,updated_at",
+    )
+
+    groups = [
+        (group_key, list(vals))
+        for group_key, vals in itertools.groupby(
+            results, key=lambda item: (item.alert.email, item.item_id, item.currency)
+        )
+    ]
+
+    # For each group get earliest and latest record
+    groups = [(group_key, (vals[0], vals[-1])) for group_key, vals in groups]
+
+    return groups
+
+
+def prepare_email_data(groups, decrease_percent: float) -> t.Dict[str, t.List[t.Dict]]:
+    # For each user create list of changed items
+    email_data = collections.defaultdict(list)
+    for group_key, (earliest_item, latest_item) in groups:
+        email, _, currency = group_key
+        old_price = earliest_item.price
+        new_price = latest_item.price
+
+        if (old_price - new_price) / old_price <= decrease_percent:
+            continue
+
+        email_data[email].append(
+            {
+                "currency": currency,
+                "old_price": old_price,
+                "new_price": new_price,
+                "title": latest_item.title,
+                "web_url": latest_item.web_url,
+            }
+        )
+
+    return email_data
+
+
+def send_emails(smtp: smtplib.SMTP, email_data: t.Dict[str, t.List[t.Dict]]):
     pass
+
+
+def price_decrease_job(alerts_client: AlertsClient, smtp: smtplib.SMTP):
+    from_dt = arrow.now().shift(**INSIGHTS_DELTA)
+    groups = select_price_minmax(from_dt=from_dt, alerts_client=alerts_client)
+    email_data = prepare_email_data(groups=groups, decrease_percent=0.02)
+    send_emails(smtp=smtp, email_data=email_data)
 
 
 @click.command(name="price_decrease_job")
