d00677ea613aac1987d41923fbc55a869ddb299c
Ladislao <vladx09rus@gmail.com>
2022-04-06 00:06:40 +0000

    Fix crud test

--- alerts/alerts/tests/test_alerts_crud.py
+++ alerts/alerts/tests/test_alerts_crud.py
@@ -14,6 +14,7 @@ class Suite(TestCase):
                 "every": 10,
                 "period": "seconds",
             },
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, 201)
 
@@ -25,27 +26,28 @@ class Suite(TestCase):
         self.assertIsNotNone(alert.task)
 
         task_id = alert.task.id
-        self.assertEqual(alert.task.every, 10)
-        self.assertEqual(alert.task.period, "seconds")
+        self.assertEqual(alert.task.interval.every, 10)
+        self.assertEqual(alert.task.interval.period, "seconds")
 
         response = c.patch(
-            f"/api/v1/alerts/{alert.id}",
+            f"/api/v1/alerts/{alert.id}/",
             {
                 "every": 20,
                 "period": "minutes",
             },
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, 200)
         alert.refresh_from_db()
         self.assertIsNotNone(alert.task)
-        self.assertEqual(alert.task.every, 20)
-        self.assertEqual(alert.task.period, "minutes")
+        self.assertEqual(alert.task.interval.every, 20)
+        self.assertEqual(alert.task.interval.period, "minutes")
         self.assertEqual(alert.task.id, task_id)
 
         response = c.delete(
-            f"/api/v1/alerts/{alert.id}",
+            f"/api/v1/alerts/{alert.id}/",
         )
         self.assertEqual(response.status_code, 204)
 
-        self.assertIsNone(models.Alert.objects.get(pk=alert_id))
-        self.assertIsNone(beat_models.PeriodicTask.objects.get(pk=task_id))
+        self.assertIsNone(models.Alert.objects.filter(pk=alert_id).first())
+        self.assertIsNone(beat_models.PeriodicTask.objects.filter(pk=task_id).first())
